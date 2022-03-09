443bcea5dedf53dff31a845967acf2139670cc70
saswat3115 <saswat3115@gmail.com>
2022-03-09 16:32:54 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import './style.css';
 import Chart from './Chart';
 
 const data = {
-  mon: { title: 'M', amount: 40, color: '#F8C630' },
-  tue: { title: 'T', amount: 50, color: 'rgba(255, 0, 0, 0.25)' },
-  wed: { title: 'W', amount: 30, color: '#F8C630' },
-  thu: { title: 'T', amount: -40, color: 'rgba(0, 156, 91, 0.25)' },
-  fri: { title: 'F', amount: -20, color: '#F8C630' },
-  sat: { title: 'S', amount: 70, color: 'rgba(255, 0, 0, 0.25)' },
-  sun: { title: 'S', amount: 30, color: 'rgba(0, 156, 91, 0.25)' },
+  1: { title: 'M', amount: 40, color: '#F8C630' },
+  2: { title: 'T', amount: 50, color: 'rgba(255, 0, 0, 0.25)' },
+  3: { title: 'W', amount: 30, color: '#F8C630' },
+  4: { title: 'T', amount: -40, color: 'rgba(0, 156, 91, 0.25)' },
+  5: { title: 'F', amount: -20, color: '#F8C630' },
+  6: { title: 'S', amount: 70, color: 'rgba(255, 0, 0, 0.25)' },
+  7: { title: 'S', amount: 30, color: 'rgba(0, 156, 91, 0.25)' },
+  8: { title: 'M', amount: 40, color: '#F8C630' },
+  9: { title: 'T', amount: 50, color: 'rgba(255, 0, 0, 0.25)' },
+  10: { title: 'W', amount: 30, color: '#F8C630' },
+  11: { title: 'T', amount: -40, color: 'rgba(0, 156, 91, 0.25)' },
 };
 
 export default function App() {
   return (
     <div className="chart">
-      <Chart data={data} />
+      <Chart data={data} selected={6} />
     </div>
   );
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const VerticalBar = ({ length, startPoint, color, title }) => {
   const moveY = startPoint.y - length;
-  console.log(moveY, startPoint.y, length);
   return (
     <g>
       <path
@@ -41,31 +40,76 @@ const VerticalCenterLine = ({ centerPoint }) => {
 
 // }
 
-const Chart = ({ data }) => {
+const Chart = ({ data, selected }) => {
+  const [current, setCurrent] = useState(selected);
+  const [startX, setStartX] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragDiff, setDragDiff] = useState(0);
+
   const H = 240;
   const W = 240;
   const centerY = 120;
   const centerX = 120;
   const GAP = 20;
-  const startPointX = centerX - Math.ceil(Object.keys(data).length / 2) * GAP;
+  const startPointX = centerX - current * GAP;
+
+  const startDrag = (e) => {
+    // if (succeeded) return;
+    setIsDragging(true);
+    if (e.touches && e.touches[0]) {
+      setStartX(e.touches[0].clientX);
+    } else {
+      setStartX(e.clientX);
+    }
+  };
+
+  const onDrag = (e) => {
+    if (isDragging) {
+      if (e.touches && e.touches[0]) {
+        // setThumbLeft(
+        // 	Math.min(
+        // 		Math.max(0, e.touches[0].clientX - startX),
+        // 		container.width - thumb.width / 2
+        // 	)
+        // );
+      } else {
+        console.log({ startX, clientX: e.clientX, dragDiff });
+        const diff = startX - e.clientX;
+        setDragDiff(-diff);
+      }
+    }
+  };
+
+  const dragEnd = () => {
+    setIsDragging(false);
+  };
 
   return (
-    <svg height="100%" width="100%" viewBox={`0 0 ${H} ${W}`}>
-      {Object.keys(data).map((item, i) => {
-        const { color, title, amount } = data[item];
-        return (
-          <VerticalBar
-            key={item}
-            color={color}
-            length={amount}
-            title={title}
-            startPoint={{
-              x: startPointX + i * GAP,
-              y: centerY,
-            }}
-          />
-        );
-      })}
+    <svg
+      height="100%"
+      width="100%"
+      viewBox={`0 0 ${H} ${W}`}
+      onMouseDown={startDrag}
+      onMouseMove={onDrag}
+      onMouseUp={dragEnd}
+    >
+      <g style={{ transform: `translateX(${dragDiff}px)` }}>
+        {Object.keys(data).map((item, i) => {
+          const { color, title, amount } = data[item];
+          return (
+            <VerticalBar
+              key={item}
+              color={color}
+              length={amount}
+              title={title}
+              startPoint={{
+                x: startPointX + i * GAP,
+                y: centerY,
+              }}
+            />
+          );
+        })}
+      </g>
       <VerticalCenterLine centerPoint={centerX - GAP} />
     </svg>
   );
